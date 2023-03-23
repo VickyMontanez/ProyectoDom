1e286925c336df3c51eb104f3c485aaf1c36e7cd
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-03-22 22:00:25 -0500

    feat: :sparkles: Add Articles Modules
    Add new features of Articles from another article, includes copyright.

---
+++ componets/myArticle.js
@@ -0,0 +1,35 @@
+export default {
+    article:[
+        {
+            title:"Arctic Monkeys: “We know more tricks now, but we’re still rolling on that same instinct”.",
+            date:"21st October 2022",
+            autor:{
+                name:"Sophie Williams",
+                href:"https://www.nme.com/big-reads/arctic-monkeys-cover-interview-2022-the-car-3332436"
+            },
+            paragraph:"After a glorious, but divisive, sonic shift, the Sheffield band double down with their lush new album ‘The Car’, proof that they’re ready to follow wherever the road takes them",
+            content:"Crucially, the new album presents both a more cohesive and collaborative band than the one we heard on 2018’s divisive ‘Tranquility Base Hotel & Casino’. That record riffed on consumerism and technology with a burnished depth, but traded it’s wildly successful predecessor’s tsunami of bravado, riffs and hairgel for searching lounge-pop.",
+            blockquote:"“We weren’t mentally ready to play stadiums up until now” – Alex Turner",
+            reference:"By throwing themselves into new, more daring sounds, Arctic Monkeys have emerged fearless, Turner says decisively:",
+            quote:" “Well, the fact we gave ourselves the name ‘Arctic Monkeys’ alludes to the extent of ambitions we had... Clearly hardly any.”",
+            additiona:"‘The Car’’s daring centrepiece, ‘Body Paint’ flips the script entirely: you can practically hear Turner wink as he sings, “and if you’re thinking of me / I’m probably thinking of you”, before swirling atmospherics and O’Malley’s tumbling bass make way for a gale-force guitar solo from Cook. It’s the full-bodied sound of the Butley Priory trip, which was solely about having fun and bringing that feeling into the new record.",
+        }
+    ],
+
+    showArticle(){
+        this.article.forEach((val,id)=>{
+            document.querySelector("#articulo").insertAdjacentHTML("beforeend",`<hr>
+            <h2 class="blog-post-title">${val.title}</h2>
+            <p class="blog-post-meta">${val.date} by <a href="${val.autor.href}" target="_blank">${val.autor.name}</a></p>
+            <p>${val.paragraph}</p>
+            <hr>
+            <p>${val.content}</p>
+            <h2>${val.blockquote}</h2>
+            <p>${val.reference}</p>
+            <blockquote class="blockquote">
+              <p>${val.quote}</p>
+            </blockquote>
+            <p>${val.additiona}</p>`)
+        })
+    }
+}
