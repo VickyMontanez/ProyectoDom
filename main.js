8ea35f91282a58ae2440d0e39a6d7696f52ca820
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-03-22 21:53:03 -0500

    feat: :sparkles: Add Articles Modules
    Add new features of Articles from another article, includes copyright.

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 import myBanner from "./componets/myBanner.js";
 import myHeader from "./componets/myHeader.js";
-import myCards from "./componets/myCards";
+import myCards from "./componets/myCards.js";
+import myArticle from "./componets/myArticle.js";
 
 myHeader.listTitle();
 myHeader.listarSongs();
 myBanner.showImage();
 myBanner.showSectionBanner();
+myCards.showCard();
+myArticle.showArticle();
