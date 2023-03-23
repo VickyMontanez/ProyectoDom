c09196650f561480712e6e8ab8c0e57853f2f768
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-03-22 21:57:54 -0500

    feat: :sparkles: Add Articles Modules
    Add new features of Articles from another article, includes copyright.

--- componets/myBanner.js
+++ componets/myBanner.js
@@ -3,7 +3,7 @@ export default{
     title : "Arctic Monkeys",
     paragraph: "Unless you've been living under a soundproof rock for the last two decades, you will have undoubtedly heard of the legendary Arctic Monkeys. Churning out hits such as 'I Bet You Look Good On The Dancefloor', 'When The Sun Goes Down' and 'R U Mine?', the band have achieved monumental world-wide fame with their ever-changing sound since they burst onto the music scene in 2005.",
     btn:{
-        name:"Continuar...",
+        name:"Continue...",
         href: "https://planetradio.co.uk/absolute-radio/music/news/arctic-monkeys/"
     },
     showImage(){
